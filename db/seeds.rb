10592d9b59ea432b97c0582f0e6379088124c1d7
Kohei SUGI <koheisg@hey.com>
2023-01-04 22:30:12 +0900

    Make 100 times

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-20.times { Task.create(name: "name#{_1}") }
+100.times { Task.create(name: "name#{_1}") }
